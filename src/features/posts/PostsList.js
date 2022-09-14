32a144f67fb924aee9385867297a48815f5fa8e3
Josip Markovic <bozadesign@gmail.com>
2022-09-14 16:48:42 +0200

    add refetch

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -8,6 +8,7 @@ import { ReactionButtons } from './ReactionButtons'
 
 import { useGetPostsQuery } from '../../api/features/apiSlice'
 import { useMemo } from 'react'
+import classNames from 'classnames'
 
 let PostExcerpt = ({ post }) => {
   return (
@@ -31,9 +32,11 @@ export const PostsList = () => {
   const {
     data: posts = [],
     isLoading,
+    isFetching,
     isSuccess,
     isError,
     error,
+    refetch,
   } = useGetPostsQuery()
 
   const sortedPosts = useMemo(() => {
@@ -48,9 +51,15 @@ export const PostsList = () => {
   if (isLoading) {
     content = <Spinner text="Loading..." />
   } else if (isSuccess) {
-    content = sortedPosts.map((post) => (
+    const renderedPosts = sortedPosts.map((post) => (
       <PostExcerpt key={post.id} post={post} />
     ))
+
+    const containerClassname = classNames('posts-container', {
+      disabled: isFetching,
+    })
+
+    content = <div className={containerClassname}>{renderedPosts}</div>
   } else if (isError) {
     content = <div>{error.toString()}</div>
   }
@@ -58,6 +67,7 @@ export const PostsList = () => {
   return (
     <section className="posts-list">
       <h2>Posts</h2>
+      <button onClick={refetch}>Refetch Posts</button>
       {content}
     </section>
   )
